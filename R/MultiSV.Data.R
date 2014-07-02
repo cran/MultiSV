dea48712dfa9942ad0180ad8156bb5d094270914
Khurram Maqbool <khurram.maqbool@outlook.com>
2014-07-02 00:00:00 +0000

    version 0.0-62

---
+++ R/MultiSV.Data.R
@@ -0,0 +1,18 @@
+##' RD Data
+##'
+##' The data set is read depth (RD) from populations of pigs (SS10.2). RD from each 
+##' population in 1kb bins.
+##'
+##' @name MultiSVData
+##' @aliases MultiSVData
+##' @docType data
+##' @author Khurram \enc{Maqbool}{Maqbool}
+##' @references Rubin, C.-J., H.-J.Megens, A.Martinez Barrio, K.Maqbool, S. Sayyab, 
+##' D. Schwochow, C.Wang, Ö. Carlborg, P. Jern, C.B. Jørgensen, A. 
+##' L.Archibald, M. Fredholm, M. A. M. Groenen, and L.Andersson. 
+##' 2012. Strong signatures of selection in the domestic pig genome. 
+##' Proc. Natl. Acad. Sci. USA 109:19529–19536, DOI: 10.1073/pnas.1217149109
+##' 
+##' @keywords MultiSV CNV Duplication Deletion Population
+NULL
+
